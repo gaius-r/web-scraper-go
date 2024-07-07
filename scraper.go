284495003ae2c81043be6f43ee9bedb34ffe7e0f
gaius-r <gaiusreji11@gmail.com>
2024-07-07 21:02:21 +0530

    refactor: added onHTML callback and csv write changes

--- scraper.go
+++ scraper.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"log"
+	"os"
 
 	// importing colly for web-scraping
 	"github.com/gocolly/colly"
 )
 
+// defining a data structure to store the scraped data
+type Product struct {
+	url, image, name, price string
+}
+
 func main() {
 	// scraping logic
+
+	var products []Product
+
 	fmt.Println("Creating Collector")
 	c := colly.NewCollector()
 	c.OnRequest(func(r *colly.Request) {
@@ -24,15 +34,60 @@ func main() {
 		fmt.Println("Page visited: ", r.Request.URL)
 	})
 
-	c.OnHTML("a", func(e *colly.HTMLElement) {
-		// printing all URLs associated with the a links in the page
-		fmt.Printf("%v", e.Attr("href"))
+	// iterating over the list of HTML product elements
+	c.OnHTML("li.product", func(e *colly.HTMLElement) {
+		// initializing a new product instance
+		product := Product{}
+
+		// scraping the data of interest
+		product.url = e.ChildAttr("a", "href")
+		product.image = e.ChildAttr("img", "src")
+		product.name = e.ChildText("h2")
+		product.price = e.ChildText(".price")
+
+		// adding the product instance with scraped data to the list of products
+		products = append(products, product)
 	})
 
 	c.OnScraped(func(r *colly.Response) {
 		fmt.Println(r.Request.URL, " scraped!")
+
+		// opening the CSV file
+		file, err := os.Create("Products.csv")
+		if err != nil {
+			log.Fatalln("Failed to create output CSV file!", err)
+		}
+		defer file.Close()
+
+		// initializing a file writer
+		writer := csv.NewWriter(file)
+
+		// writing the CSV headers
+		headers := []string{
+			"url",
+			"image",
+			"name",
+			"price",
+		}
+
+		writer.Write(headers)
+
+		// writing each product as a CSV row
+		for _, product := range products {
+			// converting a Product to an array of strings
+			record := []string{
+				product.url,
+				product.image,
+				product.name,
+				product.price,
+			}
+
+			// adding a CSV record to the output file
+			writer.Write(record)
+		}
+		defer writer.Flush()
 	})
 
-	c.Visit("https://en.wikipedia.org/wiki/Main_Page")
+	c.Visit("https://www.scrapingcourse.com/ecommerce/")
 	fmt.Println("Hello World!")
 }
